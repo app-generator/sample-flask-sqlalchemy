b7dfb9322c3a462cf39b6ad936e72be6cc9d5b55
App Generator <support@appseed.us>
2021-04-05 17:15:24 +0300

    Phase 3 - Use the new Config file
    - Variables are moved to the new file

--- app/__init__.py
+++ app/__init__.py
@@ -10,21 +10,11 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# Grabs the folder where the script runs.
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 # Inject Flask magic
 app = Flask(__name__)
 
-# App Config - the minimal footprint
-app.config['TESTING'] = True 
-
-# Set up the App SECRET_KEY
-SECRET_KEY = 'S_U_perS3crEt_KEY#9999'
-
-# SQLAlchemy Settings
-app.config['SQLALCHEMY_DATABASE_URI']        = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Load configuration
+app.config.from_object('app.config.Config')
 
 # Construct the DB Object (SQLAlchemy interface)
 db = SQLAlchemy (app)
