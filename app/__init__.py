ae60d0af017bb3dc910697a09eb09a51d1297226
App Generator <support@appseed.us>
2021-04-05 19:54:05 +0300

    Phase 4 - Integrate Flask-Migrate

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ import os
 # Import Flask 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate    import Migrate
 
 # Inject Flask magic
 app = Flask(__name__)
@@ -18,6 +19,9 @@ app.config.from_object('app.config.Config')
 
 # Construct the DB Object (SQLAlchemy interface)
 db = SQLAlchemy (app)
+
+# Enabel migration for our application
+Migrate(app, db)
 
 # Import routing to render the pages
 from app import views, models
