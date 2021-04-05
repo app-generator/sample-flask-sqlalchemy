a2fa2e15607f06ed45ce8f6f32b9aa854767d073
App Generator <support@appseed.us>
2021-04-05 19:53:46 +0300

    Phase 4 - Update Stats model
    - New field - total_sales (integer)

--- app/models.py
+++ app/models.py
@@ -7,17 +7,19 @@ from app import db
 
 class Stats(db.Model):
 
-    id         = db.Column(db.Integer,   primary_key=True )
-    month      = db.Column(db.String(64),    unique=True  )
-    sold_units = db.Column(db.Integer                     )
+    id          = db.Column(db.Integer,   primary_key=True )
+    month       = db.Column(db.String(64),    unique=True  )
+    sold_units  = db.Column(db.Integer                     )
+    total_sales = db.Column(db.Integer                     )
 
-    def __init__(self, id, month, sold_units):
-        self.id         = id
-        self.month      = month
-        self.sold_units = sold_units
+    def __init__(self, id, month, sold_units, total_sales):
+        self.id          = id
+        self.month       = month
+        self.sold_units  = sold_units
+        self.total_sales = total_sales
 
     def __repr__(self):
-        return self.month + ' - ' + str(self.sold_units)
+        return self.month + ' = ' + str(self.sold_units) + '/ ' + str(self.total_sales)
 
     # Optional helper
     def save(self):
